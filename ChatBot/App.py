3a64f3e7c84bffcdff4bddbe1d9edd8abd93b51b
saikowluru2005 <saikowluru3.com>
2026-08-12 19:46:49 +0530

    Added frontent to frunnybot

--- ChatBot/App.py
+++ ChatBot/App.py
@@ -1,18 +1,38 @@
+import streamlit as st
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
+
 load_dotenv()
 
-model=ChatGoogleGenerativeAI(model='gemini-3.6-flash')
-message=[
-    SystemMessage(content='You are a funny AI agent')
+model = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+
+# System role
+message = [
+    SystemMessage(content="You are a funny AI agent")
 ]
-print("Welcome to Bot")
-while True:
-    text=input("You: ")
-    if text==0:
-        break
+
+# Streamlit UI
+st.title("🤖 Funny AI Chatbot")
+
+# Chat input
+text = st.chat_input("Type your message...")
+
+if text:
+    # User role
     message.append(HumanMessage(content=text))
-    response=model.invoke(message)
+
+    # AI response
+    response = model.invoke(message)
+
+    # AI role
     message.append(AIMessage(content=response.text))
-    print("model: ",response.text)
+
+    # Display user message
+    with st.chat_message("user"):
+        st.write(text)
+
+    # Display AI message
+    with st.chat_message("assistant"):
+        st.write(response.text)
