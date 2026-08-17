fadb9ffa81c63d36ac7bfa2c0364374f6d921a72
saikowluru2005 <saikowluru3.com>
2026-08-17 20:54:53 +0530

    WebBaseLoader

--- RAGDocLoaders/App.py
+++ RAGDocLoaders/App.py
@@ -1,6 +1,6 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import WebBaseLoader
 
-data=TextLoader("Sample.txt")
+data=WebBaseLoader("https://www.policybazaar.com/?utm_source=affinity&utm_medium=native&utm_campaign=home3_siteplug")
 
 docs=data.load()
 print(docs)
